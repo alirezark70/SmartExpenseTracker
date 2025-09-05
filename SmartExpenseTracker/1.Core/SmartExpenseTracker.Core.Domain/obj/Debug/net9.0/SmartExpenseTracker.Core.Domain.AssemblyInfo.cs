a664aa4d8005254fcc38b5a12cf457a12463f169
alirezark70 <rezaee.alireza7098@gmail.com>
2025-09-06 02:37:17 +0330

    Add restApi to endPoint

--- SmartExpenseTracker/1.Core/SmartExpenseTracker.Core.Domain/obj/Debug/net9.0/SmartExpenseTracker.Core.Domain.AssemblyInfo.cs
+++ SmartExpenseTracker/1.Core/SmartExpenseTracker.Core.Domain/obj/Debug/net9.0/SmartExpenseTracker.Core.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartExpenseTracker.Core.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485d50903f85a393b7274a46e68fcf7f40ae475b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43528788de372120db3d865b59e8fbef07c00e6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartExpenseTracker.Core.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartExpenseTracker.Core.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
