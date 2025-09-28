05cd6cfe5aa1d20ead20c44b99beaddc0994322e
alirezark70 <rezaee.alireza7098@gmail.com>
2025-09-28 22:37:20 +0330

    apiResponse Action Filter

---
+++ SmartExpenseTracker/3.EndPoint/SmartExpenseTracker.EndPoint.RestApi/Filters/ApiResponseActionFilter.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SmartExpenseTracker.Core.Domain.DomainModels.Response.Entities;
+
+namespace SmartExpenseTracker.EndPoint.RestApi.Filters
+{
+    public class ApiResponseActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Result is ObjectResult objectResult && objectResult.Value != null)
+            {
+                var resultType = objectResult.Value.GetType();
+
+                // Check if already wrapped
+                if (resultType.IsGenericType &&
+                    (resultType.GetGenericTypeDefinition() == typeof(ApiResponse<>) ||
+                     resultType.GetGenericTypeDefinition() == typeof(PagedResponse<>)))
+                {
+                    return;
+                }
+
+                // Check for NoContent
+                if (objectResult.StatusCode == 204)
+                {
+                    context.Result = new ObjectResult(ApiResponse.NoContent())
+                    {
+                        StatusCode = 200
+                    };
+                    return;
+                }
+
+                // Wrap the response
+                var apiResponseType = typeof(ApiResponse<>).MakeGenericType(resultType);
+                var successMethod = apiResponseType.GetMethod("Success", new[] { resultType, typeof(string) });
+                var wrappedResponse = successMethod!.Invoke(null, new[] { objectResult.Value, "عملیات با موفقیت انجام شد" });
+
+                context.Result = new ObjectResult(wrappedResponse)
+                {
+                    StatusCode = objectResult.StatusCode ?? 200
+                };
+            }
+            else if (context.Result is EmptyResult || (context.Result is StatusCodeResult statusCodeResult && statusCodeResult.StatusCode == 204))
+            {
+                context.Result = new ObjectResult(ApiResponse.NoContent())
+                {
+                    StatusCode = 200
+                };
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
--- SmartExpenseTracker/3.EndPoint/SmartExpenseTracker.EndPoint.RestApi/obj/Debug/net9.0/SmartExpenseTracker.EndPoint.RestApi.AssemblyInfo.cs
+++ SmartExpenseTracker/3.EndPoint/SmartExpenseTracker.EndPoint.RestApi/obj/Debug/net9.0/SmartExpenseTracker.EndPoint.RestApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartExpenseTracker.EndPoint.RestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026d0ddc95050d020e93245974ec943a46d1ec13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a942f73e12a1c45c03542543e40623a3efb8987")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartExpenseTracker.EndPoint.RestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartExpenseTracker.EndPoint.RestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
