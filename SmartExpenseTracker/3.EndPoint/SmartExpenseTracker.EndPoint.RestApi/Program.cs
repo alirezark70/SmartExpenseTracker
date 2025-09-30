a0d99230320f75849bdfc2b33dfb963718c41590
alirezark70 <rezaee.alireza7098@gmail.com>
2025-09-30 22:15:07 +0330

    add services to endpoint program

--- SmartExpenseTracker/3.EndPoint/SmartExpenseTracker.EndPoint.RestApi/Program.cs
+++ SmartExpenseTracker/3.EndPoint/SmartExpenseTracker.EndPoint.RestApi/Program.cs
@@ -1,23 +1,15 @@
+using SmartExpenseTracker.EndPoint.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddResponseFramework();
+builder.Services.AddEndpointsApiExplorer();
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+var app = builder.ConfigureService();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.ConfigurePipeline();
 
 app.Run();
